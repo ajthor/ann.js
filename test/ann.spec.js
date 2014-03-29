267ad20cd6d4e7790be4b3896cb2908d5c28aa5c
Adam <adam.thorpe.g@gmail.com>
2014-03-29 14:24:00 -0600

    Boltzmann initial upload.
    Boltmann machine accepts values and runs them through the network to
    set states with probability. Time to start working on the learning
    algorithm.

--- test/ann.spec.js
+++ test/ann.spec.js
@@ -14,28 +14,24 @@ describe("ann", function() {
 		// var system = require("../lib/training/training.js").rprop;
 		var system = require("../lib/training/training.js").anneal;
 
-		var a = new ann([2,2,1], {trainingSystem: system});
+		var pattern = require("../lib/network/network.js").boltzmann;
+
+		var a = new ann([4,5], {
+			network: pattern,
+			trainingSystem: system
+		});
 		
-		console.log(a.run([0,0]));
-		console.log(a.run([1,0]));
-		console.log(a.run([0,1]));
-		console.log(a.run([1,1]));
+		console.log(a.run([0,0,0,0]));
+		// console.log(a.run([1,0,1,0]));
+		// console.log(a.run([0,1,0,1]));
+		// console.log(a.run([1,1,1,1]));
 
-		a.train([[0,0], [0,1], [1,0], [1,1]], [[0], [1], [1], [0]]);
-		// // a.train([[0,1]], [[1]]);
-		// // a.train([[1,0]], [[1]]);
+		// a.train([[0,0], [0,1], [1,0], [1,1]], [[0], [1], [1], [0]]);
 
-		// // console.log(a.input([0,0]));
-		// // console.log(a.input([1,0]));
-		// // console.log(a.input([0,1]));
-		// // console.log(a.input([1,1]));
-		
-		// // a.train([[0,0]], [[0]]);
-
-		console.log(a.run([0,0]));
-		console.log(a.run([1,0]));
-		console.log(a.run([0,1]));
-		console.log(a.run([1,1]));
+		// console.log(a.run([0,0]));
+		// console.log(a.run([1,0]));
+		// console.log(a.run([0,1]));
+		// console.log(a.run([1,1]));
 
 
 
