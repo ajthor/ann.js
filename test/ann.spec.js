80b243df4b8dd13271b52f07167c6aa1165edf89
Adam <adam.thorpe.g@gmail.com>
2014-03-31 13:37:42 -0600

    progress 31.3.14

--- test/ann.spec.js
+++ test/ann.spec.js
@@ -12,21 +12,60 @@ describe("ann", function() {
 
 		// var system = require("../lib/training/training.js").backprop;
 		// var system = require("../lib/training/training.js").rprop;
-		var system = require("../lib/training/training.js").anneal;
+		var system = require("../lib/training/training.js").boltzmann;
 
 		var pattern = require("../lib/network/network.js").boltzmann;
 
-		var a = new ann([4,5], {
+		var a = new ann([6,2], {
 			network: pattern,
-			trainingSystem: system
+			trainingSystem: system,
+			hasBias: false
 		});
 		
-		console.log(a.run([0,0,0,0]));
-		console.log(a.run([1,0,1,0]));
-		console.log(a.run([0,1,0,1]));
-		console.log(a.run([1,1,1,1]));
+		console.log(a.run([1,1,1,0,0,0]));
+		console.log(a.run([1,0,1,0,0,0]));
+		console.log(a.run([1,1,1,0,0,0]));
+		console.log(a.run([0,0,1,1,1,0]));
+		console.log(a.run([0,0,1,1,0,0]));
 
-		// a.train([[0,0], [0,1], [1,0], [1,1]], [[0], [1], [1], [0]]);
+		a.train([
+			[1,1,1,0,0,0],
+			[1,0,1,0,0,0],
+			[1,1,1,0,0,0],
+			[0,0,1,1,1,0],
+			[0,0,1,1,0,0]
+		]);
+		
+		console.log(a.run([1,1,1,0,0,0]));
+		console.log(a.run([1,0,1,0,0,0]));
+		console.log(a.run([1,1,1,0,0,0]));
+		console.log(a.run([0,0,1,1,1,0]));
+		console.log(a.run([0,0,1,1,0,0]));
+
+		// console.log("1",a.network.runHidden([0,1]));
+		// console.log("2",a.network.runHidden([0,1]));
+		// console.log("3",a.network.runHidden([0,1]));
+		// console.log("4",a.network.runHidden([0,1]));
+		// console.log("5",a.network.runHidden([0,1]));
+
+		// console.log("1",a.network.runHidden([1,0]));
+		// console.log("2",a.network.runHidden([1,0]));
+		// console.log("3",a.network.runHidden([1,0]));
+		// console.log("4",a.network.runHidden([1,0]));
+		// console.log("5",a.network.runHidden([1,0]));
+
+		// Training set for XOR problems.
+		// a.train([
+		// 	[0,0], 
+		// 	[0,1], 
+		// 	[1,0], 
+		// 	[1,1]
+		// ], [
+		// 	[0], 
+		// 	[1], 
+		// 	[1], 
+		// 	[0]
+		// ]);
 
 		// console.log(a.run([0,0,0,0]));
 		// console.log(a.run([1,0,1,0]));
