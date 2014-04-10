a96405975cd7f4aa7f68bea436ae3c05ea2f4be1
Adam <adam.thorpe.g@gmail.com>
2014-04-09 18:07:15 -0600

    Updated tests.

--- test/ann.spec.js
+++ test/ann.spec.js
@@ -1,12 +1,10 @@
-cs = require("coffee-script");
-
 describe("ann", function() {
 
 	var ann;
 	
 	it("should load without throwing", function() {
 		expect(function() {
-			ann = require("../lib/pattern/perceptron");
+			ann = require("../lib/pattern/perceptron.js");
 		}).not.toThrow();
 	});
 
@@ -14,38 +12,38 @@ describe("ann", function() {
 
 	it("instantiation should work", function() {
 
-		// var system = require("../lib/training/system/backprop");
+		var system = require("../lib/training/system/anneal.js");
 
 
-		var a = new ann([2,2,2,1]);
+		var a = new ann([2,2,3,1]);
 
-		// var anneal = new system(a);
+		var anneal = new system(a);
 
 		// var system = require("../lib/training/training.js").backprop;
 		// var system = require("../lib/training/training.js").rprop;
 
 		// var a = new ann([2,2,1], {trainingSystem: system});
 		
-		console.log(a.run([0,0]));
-		console.log(a.run([1,0]));
-		console.log(a.run([0,1]));
-		console.log(a.run([1,1]));
+		console.log(a.run([0,0]).value);
+		console.log(a.run([1,0]).value);
+		console.log(a.run([0,1]).value);
+		console.log(a.run([1,1]).value);
 
-		// anneal.train(
-		// [	[0,0], 
-		// 	[0,1], 
-		// 	[1,0], 
-		// 	[1,1]], 
-		// [	[0], 
-		// 	[1], 
-		// 	[1], 
-		// 	[0]]);
+		anneal.train(
+		[	[0,0], 
+			[0,1], 
+			[1,0], 
+			[1,1]], 
+		[	[0], 
+			[1], 
+			[1], 
+			[0]]);
 
 		
-		console.log(a.run([0,0]));
-		console.log(a.run([1,0]));
-		console.log(a.run([0,1]));
-		console.log(a.run([1,1]));
+		console.log(a.run([0,0]).value);
+		console.log(a.run([1,0]).value);
+		console.log(a.run([0,1]).value);
+		console.log(a.run([1,1]).value);
 
 
 
