6a7edfe9679bdf6a84fe63fe9013ba9d6c3880e0
Adam <adam.thorpe.g@gmail.com>
2014-06-23 10:32:15 -0600

    create: repl

---
+++ bin/repl.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// Ann.node REPL
+// =============
+// This is a simplistic REPL which exposes the Ann.node object to 
+// the user for debugging and functional testing purposes.
+var repl = require('repl');
+var ann = require('../build/Release/ann');
+
+console.log('Starting Ann.node REPL...');
+console.log('To exit, press <Cmd+D>');
+
+// Start the REPL.
+var r = repl.start({
+	prompt: '> '
+});
+
+// Expose the math and instance objects to the REPL.
+r.context.ann = ann;
+r.context.i = new ann();
+
+// Print a message to the console on exit.
+r.on('exit', function () {
+	console.log('Exit Ann.node REPL.');
+});
